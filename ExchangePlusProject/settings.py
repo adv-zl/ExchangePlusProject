5120b11d1afa792acc7e72462deca6465498a0e6
Andrei_Drang <drang.andray@gmail.com>
2017-07-27 01:40:39 +0300

    Немного переделана разметка. Добавлены варнинги. Изменена админка на Django Suit.

--- ExchangePlusProject/settings.py
+++ ExchangePlusProject/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,10 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, '/static/')
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+
 SESSION_COOKIE_AGE = 2*3600
 
 
-
-
+STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
